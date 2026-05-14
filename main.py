45567b2a2cc103997adada7e08d7d2dcbeafd1fb
Antix7 <antekklejman@gmail.com>
2026-05-14 05:02:54 +0200

    Implemented zooming and panning of the map

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from math import *
 import pygame as pg
-from pygame.constants import QUIT
+from pygame.constants import *
 
 MAP_SCALE = 2400*18/pi #[px/rad]
 MAP_LAT_START = (pi/2)*(4/9)
@@ -12,26 +12,55 @@ def project_spherical(latitude, longitude):
     return x, y
 
 pg.init()
-window = pg.display.set_mode((1000, 500))
-game_map = pg.transform.scale_by(pg.image.load("data/game_map.jpg").convert(), 1/25)
-map_height = game_map.get_rect().height
 
-lat = (59+48/60+53.23/3600)*(pi/180)
-lon = -(43 + 35/60 + 08.25/3600)*(pi/180)
+display_info = pg.display.Info()
+window_width = int(display_info.current_w * 0.9)
+window_height = int(display_info.current_h * 0.8)
 
+screen = pg.display.set_mode((window_width, window_height))
+pg.display.set_caption("Viking Explorers")
+map_image = pg.image.load("data/game_map.jpg").convert()
 
+zoom_level = 0.05
+zoom_factor = 1.2
+camera_x = 0
+camera_y = 0
+lmb_held_down = False
 
-window.blit(game_map, (0, 0))
-x, y = project_spherical(lat, lon)
-x /= 25; y /= 25
-print(x, y)
-print(map_height)
-pg.draw.circle(window, "red", (x, map_height-y), 3)
-pg.display.flip()
 run = True
 while run:
     for event in pg.event.get():
         if event.type == QUIT:
             run = False
+
+        # Zooming of the map
+        if event.type == MOUSEWHEEL:
+            zoom_exponent = 0
+            if event.y > 0:
+                zoom_exponent = 1
+            elif event.y < 0:
+                zoom_exponent = -1
+            zoom_level *= zoom_factor**zoom_exponent
+            mouse_x, mouse_y = pg.mouse.get_pos()
+            camera_x = mouse_x + (camera_x - mouse_x) * (zoom_factor**zoom_exponent)
+            camera_y = mouse_y + (camera_y - mouse_y) * (zoom_factor**zoom_exponent)
+
+    # Panning of the map
+    if pg.mouse.get_pressed()[0]:
+        if lmb_held_down:
+            mouse_dx, mouse_dy = pg.mouse.get_rel()
+            camera_x += mouse_dx
+            camera_y += mouse_dy
+        else:
+            lmb_held_down = True
+            pg.mouse.get_rel()
+    else:
+        lmb_held_down = False
+
+    screen.fill((0, 0, 0))
+    scaled_map = pg.transform.smoothscale_by(map_image, zoom_level)
+    screen.blit(scaled_map, (camera_x, camera_y))
+
+    pg.display.flip()
 
 pg.quit()
